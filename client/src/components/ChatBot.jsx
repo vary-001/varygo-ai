3b561b5aa250cc2cfacfd3edabb997b57bc07467
Vary Patrick <varypatrick8@gmail.com>
2025-10-12 19:12:31 +0200

    changed loacl host to online live url host

--- client/src/components/ChatBot.jsx
+++ client/src/components/ChatBot.jsx
@@ -26,7 +26,7 @@ function ChatBox({ user, isMobile }) {
   // API call
   const sendMessageToAPI = async (message) => {
     try {
-      const response = await fetch('http://localhost:5000/api/chat', {
+      const response = await fetch('https://varygo-ai.onrender.com/api/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message }),
